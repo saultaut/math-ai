ecfea4d84f5d51b1c1505478107b5da879c0a85c
Saulius <saulius.tautvaisas@gmail.com>
2024-03-08 10:20:25 +0200

    verifier eval code and files

--- scripts/metamath/eval_step_beam.sh
+++ scripts/metamath/eval_step_beam.sh
@@ -4,21 +4,20 @@ generator_id=MetaMath-Mistral-7B
 verifier_id=n100-scahead-mse-lm-token
 
 n_beam=4
-n_sampling_steps=5
+n_sampling_steps=4
 
 
 model_name_or_path=facebook/opt-125m
 verifier_model_name_or_path=./models/metamath/verifiers/MetaMath-Mistral-7B-1
 
 # --config_file ./configs/multi_gpu_inference.yaml \
-accelerate launch \
-  eval_generator_by_step_metamath.py \
+accelerate launch eval_generator_by_step_metamath.py \
   --model_name_or_path ${model_name_or_path} \
   --verifier_model_name_or_path ${verifier_model_name_or_path} \
-  --dataset MATH_test_100 \
+  --dataset metamath \
   --data_dir data/metamath \
   --output_dir eval_results/metamath/generator_with_verifier \
-  --target_set test_100 \
+  --target_set train_50 \
   --inference_mode beam \
   --batch_size 8 \
   --vs_batch_size 8 \
